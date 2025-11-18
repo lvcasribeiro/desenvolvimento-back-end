f5a505c6eac51639bda3813afc4a8d89609dd100
vieiran55 <vieiran55@gmail.com>
2025-11-17 21:25:24 -0300

    merge

--- src/main/java/projeto_garcom/com/demo/pagamento/PagamentoEnum.java
+++ src/main/java/projeto_garcom/com/demo/pagamento/PagamentoEnum.java
@@ -1,5 +1,4 @@
 package projeto_garcom.com.demo.pagamento;
 
 public enum PagamentoEnum {
-    CARTAO, DINHEIRO, CHEQUE
 }
--- src/main/java/projeto_garcom/com/demo/pagamento/dto/PagamentoRequestDTO.java
+++ src/main/java/projeto_garcom/com/demo/pagamento/dto/PagamentoRequestDTO.java
@@ -1,7 +1,2 @@
 package projeto_garcom.com.demo.pagamento.dto;
 
-import projeto_garcom.com.demo.pagamento.PagamentoEnum;
-
-public record PagamentoRequestDTO(
-        PagamentoEnum pagamento
-) {}
